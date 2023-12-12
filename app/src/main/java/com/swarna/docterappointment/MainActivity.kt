a8948be902d753182c772433e9786be513d62218
Aasritha <swarnababyashritha@gmail.com>
2023-12-12 16:30:23 +0000

    added comments

--- app/src/main/java/com/swarna/docterappointment/MainActivity.kt
+++ app/src/main/java/com/swarna/docterappointment/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.swarna.docterappointment.app.DocterApps
 
+//main activity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
